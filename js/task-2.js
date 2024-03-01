285e3a9c0ffd346ca7d58a01e4e5300e5312348f
Vladyslav Mishchuk <115806690+Vladok05@users.noreply.github.com>
2024-03-01 00:45:35 +0000

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,39 @@
+`use strict`;
+// Напиши функцію calcAverageCalories(days), яка повертає середньодобове значення кількості калорій, які спортсмен споживав протягом тижня.
+// Функція очікує один параметр: days — масив об’єктів.
+// Кожен об’єкт описує день тижня та The number of calories calories, спожитих спортсменом, у цей день.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.У консоль будуть виведені результати її викликів.
+
+const calcAverageCalories = days => {
+  let weeklyAverageCaloriesOfWeek = 0;
+  for (const day of days) {
+    weeklyAverageCaloriesOfWeek += day.calories / days.length;
+  }
+  return weeklyAverageCaloriesOfWeek;
+};
+console.log('task-2');
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
